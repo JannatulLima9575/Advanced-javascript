7b67a145f83e3e4bad80381f2c549798b4537d42
Jannatul <limajannat1675@gmail.com>
2025-02-20 19:58:33 +0600

    added arrow.js

---
+++ module-5/arrow-function.js
@@ -0,0 +1,15 @@
+// Arrow Function 
+// # basic syntax of Arrow function
+/* const add = ( ) => {
+    return a + b
+} */
+const add = (a, b) => a + b;
+console.log(add(20, 30));
+
+let greet = (name) => { 
+    return `Hello, ${name}`; 
+};
+console.log(greet("Lima"));
+
+let multiply = (a, b) => a * b;
+console.log(multiply(5, 5)); 
